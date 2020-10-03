03f1d7ad9251f8c83528b7ffa88e60ac847174c9
André Alves <andremralves@gmail.com>
2020-10-03 20:24:10 -0300

    Criação do Servidor e Algumas rotas para testes

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/lojas/:id', (req,res)=>{
+    
+})
+
+app.get('/produtos', (req,res)=>{
+    
+})
+
+app.listen('3000', ()=>{
+    console.log('servidor está funcionando rocha gay: http://localhost:3000/')
+})
+
