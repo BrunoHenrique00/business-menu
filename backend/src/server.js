86dc6a9e453023ff3fea8be62cb9fb6fbb9588bf
Bruno Henrique <bh.bsb2011@hotmail.com>
2020-10-29 19:03:44 -0300

    adding cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const productRoutes = require('./routes/productRoutes.js')
 const lojasRoutes = require('./routes/lojasRoutes.js')
+const cors = require('cors')
 
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 app.use('/lojas', lojasRoutes )
 app.use('/produtos', productRoutes )
